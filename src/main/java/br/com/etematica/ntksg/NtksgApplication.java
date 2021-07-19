99be1410c24df4dd4642a699c34f60d5a0969509
nicolau <nicolau@etematica.com.br>
2021-07-18 23:21:27 -0300

    initial version

---
+++ src/main/java/br/com/etematica/ntksg/NtksgApplication.java
@@ -0,0 +1,13 @@
+package br.com.etematica.ntksg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtksgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtksgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/etematica/ntksg/NtksgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etematica.ntksg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtksgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
