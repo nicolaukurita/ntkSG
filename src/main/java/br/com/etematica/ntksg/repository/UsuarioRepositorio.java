df08a21902df2b6d91f50d086c49116421dd4e06
nicolau <nicolau@etematica.com.br>
2021-07-27 23:37:23 -0300

    User Repository class

--- src/main/java/br/com/etematica/ntksg/repository/UsuarioRepositorio.java
+++ src/main/java/br/com/etematica/ntksg/repository/UsuarioRepositorio.java
@@ -2,8 +2,10 @@ package br.com.etematica.ntksg.repository;
 
 import br.com.etematica.ntksg.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+@Repository
+public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
 
     Usuario findUsuarioById(Integer id);
 }
