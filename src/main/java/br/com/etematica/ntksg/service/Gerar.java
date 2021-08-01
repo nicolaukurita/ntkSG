62119391a74fd619466c2ac9c660fa47307a8668
nicolau <nicolau@etematica.com.br>
2021-08-01 18:37:17 -0300

    fix optional

--- src/main/java/br/com/etematica/ntksg/service/Gerar.java
+++ src/main/java/br/com/etematica/ntksg/service/Gerar.java
@@ -2,9 +2,13 @@ package br.com.etematica.ntksg.service;
 
 import br.com.etematica.ntksg.model.Projeto;
 import br.com.etematica.ntksg.repository.ProjetoRepositorio;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.util.Optional;
 
 public class Gerar {
@@ -14,21 +18,38 @@ public class Gerar {
     ProjetoRepositorio projetoRepositorio;
 
     public void GerarProjeto(Integer id) {
-        Optional<Projeto> projeto = projetoRepositorio.findById(id);
-        GerarPreProjeto(projeto);
-        GerarMeioProjeto(projeto);
-        GerarPosProjeto(projeto);
+        Optional<Projeto> resultado = projetoRepositorio.findById(id);
+        if (resultado.isPresent()) {
+            Projeto projeto = resultado.get();
+            GerarPreProjeto(projeto);
+            GerarMeioProjeto(projeto);
+            GerarPosProjeto(projeto);
+        }
     }
 
-    private void GerarPreProjeto(Optional<Projeto> projeto) {
-        File newDirectory = new File(TEMP_DIRECTORY, "teste");
+    private void GerarPreProjeto(Projeto projeto) {
+        File newDirectory = new File(TEMP_DIRECTORY, projeto.getNome());
     }
 
-    private void GerarMeioProjeto(Optional<Projeto> projeto) {
+    private void GerarMeioProjeto(Projeto projeto) {
+        VelocityEngine velocityEngine = new VelocityEngine();
 
+        try {
+            velocityEngine.init();
+
+            Template t = velocityEngine.getTemplate("index.vm");
+
+            VelocityContext context = new VelocityContext();
+            context.put("name", "World");
+
+            StringWriter writer = new StringWriter();
+            t.merge(context, writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void GerarPosProjeto(Optional<Projeto> projeto) {
+    private void GerarPosProjeto(Projeto projeto) {
 
     }
 
