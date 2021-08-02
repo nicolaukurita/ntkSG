37619b6408ae7e29e69ec6d4499901f741f0ccf8
nicolau <nicolau@etematica.com.br>
2021-08-01 23:19:10 -0300

    Add to call Gerar

--- src/main/java/br/com/etematica/ntksg/service/Gerar.java
+++ src/main/java/br/com/etematica/ntksg/service/Gerar.java
@@ -1,24 +1,40 @@
 package br.com.etematica.ntksg.service;
 
 import br.com.etematica.ntksg.model.Projeto;
+import br.com.etematica.ntksg.model.Tarefa;
 import br.com.etematica.ntksg.repository.ProjetoRepositorio;
+import br.com.etematica.ntksg.repository.TarefaRepositorio;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.StringWriter;
 import java.util.Optional;
+import java.util.Set;
 
+@Service
 public class Gerar {
     private static final File TEMP_DIRECTORY = new File(System.getProperty("java.io.tmpdir"));
 
     @Autowired
     ProjetoRepositorio projetoRepositorio;
 
-    public void GerarProjeto(Integer id) {
-        Optional<Projeto> resultado = projetoRepositorio.findById(id);
+    @Autowired
+    TarefaRepositorio tarefaRepositorio;
+
+    public void GerarProjetos() {
+        Set<Tarefa> tarefas = tarefaRepositorio.findTarefaBySituacao("PENDENTE");
+        for (Tarefa tarefa :
+                tarefas) {
+            GerarProjeto(tarefa);
+        }
+    }
+
+    public void GerarProjeto(Tarefa tarefa) {
+        Optional<Projeto> resultado = projetoRepositorio.findById(tarefa.getProjetoId());
         if (resultado.isPresent()) {
             Projeto projeto = resultado.get();
             GerarPreProjeto(projeto);
@@ -44,6 +60,7 @@ public class Gerar {
 
             StringWriter writer = new StringWriter();
             t.merge(context, writer);
+            System.out.println(t);
         } catch (Exception e) {
             e.printStackTrace();
         }
