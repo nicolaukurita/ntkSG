190d155d12f5614797ef8f59268096406f82e0d2
nicolau <nicolau@etematica.com.br>
2021-08-01 23:16:53 -0300

    Added @Table

--- src/main/java/br/com/etematica/ntksg/model/Projeto.java
+++ src/main/java/br/com/etematica/ntksg/model/Projeto.java
@@ -5,12 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Set;
 
+@Table(name = "Projeto")
 @Entity
 @Getter
 @Setter
