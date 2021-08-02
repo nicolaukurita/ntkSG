3b09a83c25bf2ad4c1edf117abb480101554375b
nicolau <nicolau@etematica.com.br>
2021-08-01 23:17:20 -0300

    Added @EnableScheduling

---
+++ src/main/java/br/com/etematica/ntksg/NtkSgConfigurator.java
@@ -0,0 +1,20 @@
+package br.com.etematica.ntksg;
+
+import br.com.etematica.ntksg.service.Gerar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class NtkSgConfigurator {
+    @Autowired
+    Gerar gerar;
+
+    @Scheduled(fixedDelay = 1000)
+    public void gerar(){
+        System.out.println("Fixed delay " + System.currentTimeMillis() / 1000);
+        gerar.GerarProjetos();
+    }
+}
