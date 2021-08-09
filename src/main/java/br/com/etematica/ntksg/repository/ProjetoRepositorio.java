6e70d443c0c35118670f7f6a7c37149bf07885db
nicolau <nicolau@etematica.com.br>
2021-08-09 13:00:49 -0300

    remove not used import

--- src/main/java/br/com/etematica/ntksg/repository/ProjetoRepositorio.java
+++ src/main/java/br/com/etematica/ntksg/repository/ProjetoRepositorio.java
@@ -4,8 +4,6 @@ import br.com.etematica.ntksg.model.Projeto;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
 @Repository
 public interface ProjetoRepositorio extends CrudRepository<Projeto, Integer> {
 }
