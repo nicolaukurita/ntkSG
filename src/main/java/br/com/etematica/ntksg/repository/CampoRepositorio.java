3b0b9ef7b0f92291a087800d44b785bfc6f3291b
nicolau <nicolau@etematica.com.br>
2025-11-21 14:43:52 -0300

    Changed from CrudRepository to JpaRepository

--- src/main/java/br/com/etematica/ntksg/repository/CampoRepositorio.java
+++ src/main/java/br/com/etematica/ntksg/repository/CampoRepositorio.java
@@ -1,12 +1,12 @@
 package br.com.etematica.ntksg.repository;
 
 import br.com.etematica.ntksg.model.Campo;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CampoRepositorio extends CrudRepository<Campo,Integer> {
+public interface CampoRepositorio extends JpaRepository<Campo,Integer> {
     List<Campo> findAllCampoByEntidade(Integer id);
 }
--- src/main/java/br/com/etematica/ntksg/repository/EntidadeRepositorio.java
+++ src/main/java/br/com/etematica/ntksg/repository/EntidadeRepositorio.java
@@ -1,12 +1,12 @@
 package br.com.etematica.ntksg.repository;
 
 import br.com.etematica.ntksg.model.Entidade;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EntidadeRepositorio extends CrudRepository<Entidade, Integer> {
+public interface EntidadeRepositorio extends JpaRepository<Entidade, Integer> {
     List<Entidade> findAllEntidadesByProjeto(Integer projeto);
 }
